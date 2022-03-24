c7c4903e0234c002f47601634481b8ce11e3957b
Raj <rajkhulna@outlook.com>
2022-03-24 15:36:55 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Khalid Khan on 24/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
